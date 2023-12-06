b4a3f25c5f99e12d5ee446775f640620ce68dbae
Max Schlienz <50174475+MaxCanNotCode@users.noreply.github.com>
2023-12-06 15:16:14 +0100

    part1

--- 06thOfDecember/Part1/Main.java
+++ 06thOfDecember/Part1/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         int []time = {62,73,75,65};
-        int []distance ={644,1023,1240, 1023};
+        int []distance ={644,1023,1240,1023};
         int count = 1;
         for (int i = 0; i<time.length;i++){
             count = count*check(time[i], distance[i]);
@@ -10,6 +10,14 @@ public class Main {
     }
 
     private static int check(int time, int distance) {
-        
+        int count = 0;
+        int race;
+        for(int i = 0; i<=time; i++){
+            race = i * (time -i);
+            if(race>distance){
+                count++;
+            }
+        }
+        return count;
     }
 }
