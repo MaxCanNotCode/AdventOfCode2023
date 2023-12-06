5e27061c8a98b74531385f5a35c720a7ad6ce881
Max Schlienz <50174475+MaxCanNotCode@users.noreply.github.com>
2023-12-06 15:01:55 +0100

    1

---
+++ 06thOfDecember/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int []time = {62,73,75,65};
+        int []distance ={644,1023,1240, 1023};
+        int count = 1;
+        for (int i = 0; i<time.length;i++){
+            count = count*check(time[i], distance[i]);
+        }
+        System.out.println(count);
+    }
+
+    private static int check(int time, int distance) {
+        
+    }
+}
