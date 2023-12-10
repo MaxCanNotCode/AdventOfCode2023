b02437242a31988f1a1b3222e98f57d39dedc2fd
Max Schlienz <50174475+MaxCanNotCode@users.noreply.github.com>
2023-12-10 04:54:38 +0100

    LCM
    L oser
    C uck
    M outhbreather

--- 08thOfDecember/part2/Main.java
+++ 08thOfDecember/part2/Main.java
@@ -4,10 +4,10 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) throws IOException {
         String input = args[0];
-        int output = read(input);
+        long output = read(input);
         System.out.println(output);
     }
-    private static int read(String input) throws IOException {
+    private static long read(String input) throws IOException {
         String[] strArr = new String[17576];
         int[] bitmap = new int [17576];
         String instr="AMK";
@@ -27,88 +27,82 @@ public class Main {
             }
             count++;
         }
-        return counter(strArr, instr, bitmap);
-    }
-
-    private static int counter(String[] strArr, String fuckfuck, int[] bitmap) {
-        int[] ghostArr;
-        ArrayList<Integer> ghostList = new ArrayList<>();
-        ArrayList<Integer> zList = new ArrayList<>();
+        ArrayList<Integer> aList = new ArrayList<>();
         for(int i= 0; i< bitmap.length; i= i + 26){
             if(bitmap[i]==1){
-                ghostList.add(i);
+                aList.add(i);
             }
         }
+        ArrayList<Integer> zList = new ArrayList<>();
         for(int i=25; i< bitmap.length;i= i +26){
             if(bitmap[i]==1){
                 zList.add(i);
             }
         }
-        int [] zbitmap = new int[bitmap.length];
-        for (int i = 0; i< zList.size(); i++){
-            zbitmap[zList.get(i)]=1;
+        int[] zMap = new int[17576];
+        for (int i = 0; i<zList.size();i++){
+            if(bitmap[zList.get(i)]==1){
+                zMap[zList.get(i)]=1;
+            }
         }
-        ghostArr=ghostList.stream().mapToInt(Integer::intValue).toArray();
+        int[] dist = new int [aList.size()];
+        for(int i  =0; i< aList.size(); i++){
+            int start = aList.get(i);
+            dist[i]=counter(strArr, instr, bitmap, zMap, start);
+        }
+
+        return calculateLCM(dist);
+    }
+
+    private static int counter(String[] strArr, String fuckfuck, int[] bitmap, int[] zMap, int start) {
         int count = 0;
-        int [] updateArr;
-        ArrayList<Integer> updateList = new ArrayList<>();
-        updateList.addAll(ghostList);
-        updateList.add(0);
-        updateList.add(0);
-        updateArr=updateList.stream().mapToInt(Integer::intValue).toArray();
         char [] instr = fuckfuck.toCharArray();
-        int check = 0;
-        int start = 0; // AAA
-        while (check == 0){
-            for (int i = 0; i< instr.length; i++){
-                if(start != 0 && zbitmap[start]==1){
-                    updateArr=zCheck(ghostArr, count, strArr, instr, updateArr);
-                    if(updateArr[ghostArr.length]==0){
-                        return count;
-                    }
+        while (count!=-1){
+            for(int i = 0; i<instr.length; i++){
+                if(zMap[start]==1){
+                    return count;
                 }
-                String [] temp = strArr[start].split(",");
+                String[] tmp = strArr[start].split(",");
                 if(instr[i]=='L'){
-                    start = toNumb(temp[0]);
-                    count++;
+                    start = toNumb(tmp[0]);
                 } else {
-                    start = toNumb(temp[1]);
-                    count++;
+                    start = toNumb(tmp[1]);
                 }
+                count++;
             }
         }
         return count;
     }
-    private static int[] zCheck(int[] ghostArr, int count, String[] strArr, char[] instr, int[] updateArr) {
-        int inCount= 0;
-        int newCount =count - updateArr[ghostArr.length+1];
-        int checkVar = 0;
-        for (int i = 0; i< ghostArr.length; i++){
-            int start = updateArr[i];
-            for (int j = 0; j<=newCount;j++){
-                if(j>=instr.length){
-                    inCount=0;
-                }
-                String [] tmp =strArr[start].split(",");
-                if(instr[inCount]=='L'){
-                    start =toNumb(tmp[0]);
-                } else {
-                    start = toNumb(tmp[1]);
-                }
-                inCount++;
-            }
-            updateArr[i]=start;
-            if(start%25!=0){
-                checkVar++;
-            }
-        }
-        updateArr[ghostArr.length]=checkVar;
-        updateArr[ghostArr.length+1]=count;
-        return updateArr;
-    }
 
     private static int toNumb(String s) {
         char[] charArr = s.toCharArray();
-        return (676*(charArr[2] - 'A')) +(26*(charArr[1] - 'A')) +(charArr[0] - 'A');
+        return (26 * 26 * (charArr[0] - 'A')) + (26 * (charArr[1] - 'A')) + (charArr[2] - 'A');
     }
+    private static long calculateLCM(int[] numbers) {
+        if (numbers.length == 0) {
+            throw new IllegalArgumentException("Array must contain at least one element");
+        }
+
+        long lcm = numbers[0];
+
+        for (int i = 1; i < numbers.length; i++) {
+            lcm = calculateLCM(lcm, numbers[i]);
+        }
+
+        return lcm;
+    }
+
+    private static long calculateLCM(long a, long b) {
+        return Math.abs(a * b) / calculateGCD(a, b);
+    }
+
+    private static long calculateGCD(long a, long b) {
+        while (b != 0) {
+            long temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return Math.abs(a);
+    }
+
 }
